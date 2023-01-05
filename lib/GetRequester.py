3d62b4432c40917ce2eb7aba536f23208d40dbfa
Tom Tobar <cocdetombomb@gmail.com>
2023-01-05 15:03:56 -0600

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,8 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        resp = requests.get(self.url)
+        return resp.content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
